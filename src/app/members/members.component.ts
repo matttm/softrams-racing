0c6b1a396bd76ee824bdeeddc9b6fcafec2fe3ef
matttm <matttmaloney@gmail.com>
2021-02-06 18:05:41 -0500

    added deletion listener code

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { AppService } from '../app.service';
 import { Router } from '@angular/router';
+import {takeUntil} from 'rxjs/operators';
+import {Subject} from 'rxjs';
 
 @Component({
   selector: 'app-members',
   templateUrl: './members.component.html',
   styleUrls: ['./members.component.css']
 })
-export class MembersComponent implements OnInit {
+export class MembersComponent implements OnInit, OnDestroy {
   members = [];
+  unsub$: Subject<any>;
 
-  constructor(public appService: AppService, private router: Router) {}
+  constructor(public appService: AppService, private router: Router) {
+    this.unsub$ = new Subject<any>();
+  }
 
   ngOnInit() {
     this.appService.getMembers().subscribe(members => (this.members = members));
+  }
+
+  ngOnDestroy(): void {
+    this.unsub$.next();
+    this.unsub$.complete();
   }
 
   goToAddMemberForm() {
@@ -33,6 +43,8 @@ export class MembersComponent implements OnInit {
   }
 
   deleteMemberById(id: number) {
-    // this.appService.deleteMemberById(id);
+    this.appService.deleteMember(id).pipe(takeUntil(this.unsub$)).subscribe(() => {
+      console.log(`Deleted member with id: ${id}`);
+    });
   }
 }
